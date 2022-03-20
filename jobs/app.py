4b6c1ec192450f75311d74a46488efc2eb1d43b8
pmohanur <priyadharsini.mohanur@gmail.com>
2022-03-20 18:49:42 +0000

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+import _sqlite3
+from flask import Flask, render_template, g
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
+def open_connection() :
+    getattr('_connection',g,None)
 
 @app.route('/')
 @app.route('/jobs')
